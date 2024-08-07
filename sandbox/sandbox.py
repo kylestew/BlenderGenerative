7bd76a95f10d20702fcbadf42ac9b5991b97cf0a
Kyle Stewart <kylestew@gmail.com>
2024-08-07 22:23:54 +0100

    ...

--- sandbox/sandbox.py
+++ sandbox/sandbox.py
@@ -22,9 +22,22 @@ xs = np.random.uniform(0, 1, size=COUNT)
 lines = [make_line(x, y_pos, height) for x in xs]
 
 
+poly = dat.Polygon([[0, 0], [1, 0], [1, 1]])
+
+count = 10_000
+
+pts = ops.scatter_pts(poly, count)
+
+print("PTS CREATED:", len(pts))
+
+
+# %%
 ## == DISPLAY ===================================================================
 ctx.setup(900, 900, clear_color=[0, 0, 100], range=[-0.1, 1.1])
 
 ctx.draw(lines, attribs={ctx.STROKE: [0.0, 0.0, 0.0, 0.5], ctx.LINE_WIDTH: 0.01})
+
+ctx.draw(poly, attribs={ctx.STROKE: [1.0, 0.0, 0.0, 0.5], ctx.LINE_WIDTH: 1.0})
+ctx.draw(pts, attribs={ctx.STROKE: [1, 1, 1, 0.8]})
 
 ctx.display()
